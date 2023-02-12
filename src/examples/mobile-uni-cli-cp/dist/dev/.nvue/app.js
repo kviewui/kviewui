1f63cd5178e6745d708dd73414a3884df0aa5984
Yunlai Luo <kviewui>
2023-02-12 15:21:40 +0800

    feat: 删除examples

--- src/examples/mobile-uni-cli-cp/dist/dev/.nvue/app.js
+++
@@ -1,6 +0,0 @@
-Promise.resolve("./pages/components/button/index.js").then(() => {
-});
-Promise.resolve("./pages/test/index.js").then(() => {
-});
-Promise.resolve("./app.css.js").then(() => {
-});
--- src/examples/mobile-uni-cli-cp/dist/dev/mp-weixin/node-modules/@kviewui/kviewui/src/image/index.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-const common_vendor = require("../../../../../common/vendor.js");
-wx.createComponent(common_vendor.Component$3);
